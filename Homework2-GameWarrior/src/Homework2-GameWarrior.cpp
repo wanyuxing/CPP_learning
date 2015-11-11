484ed0671673283a2cbd59fe4b04f30ea75802d3
wanyuxing <wanyuxing@hotmail.com>
2015-11-12 00:29:09 +0800

    Henry Wan

--- Homework2-GameWarrior/src/Homework2-GameWarrior.cpp
+++ Homework2-GameWarrior/src/Homework2-GameWarrior.cpp
@@ -1,41 +1,29 @@
 //============================================================================
-// Name        : Testing.cpp
+// Name        : Homework2-GameWarrior.cpp
 // Author      : Henry Wan
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Word in C++, Ansi-style
+// Description : Produce Warrior in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 class Warrior
 {
 private:
-	int num, life, attack, pos;
+	int num, life, attack;
 	string type;
 	const static string CHOICE[5];
-	static int count[5];
 public:
 	Warrior(int l, int i = 0, int j = 0, int k = 0)
-	{
-		num = i; life = j; attack = k; type = CHOICE[l]; pos = l;
-		count[l]++;
-	}
-	Warrior(Warrior& a)
-	{
-		num = a.getNum(); life = a.getLife(); attack = a.getAtt();
-		pos = a.getPos(); type = a.getName();
-		count[pos]++;
-	}
+	{ num = i; life = j; attack = k; type = CHOICE[l]; }
 	friend class Foundation;
 	int getNum() { return num; }
-	int getTot() { return count[pos]; }
 	int getLife() { return life; }
 	int getAtt() { return attack; }
-	int getPos() { return pos; }
 	string getName() { return type; }
-	~Warrior() { count[pos]--; }
 };
 
 class Foundation
@@ -51,7 +39,6 @@ public:
 	{ resource = max(resource - war.getLife(), 0); }
 };
 
-int Warrior::count[5] = {0};
 const string Warrior::CHOICE[5] = {"dragon", "ninja", "iceman", "lion", "wolf" };
 const string Foundation::CHOICE[2] = {"blue", "red"};
 const string Foundation::SEQ[2] = {"blue", "red"};
@@ -81,35 +68,48 @@ int main() {
 	{
 		cout << "Case:" << i + 1 << endl;
 		Warrior Dragon(0, 0, test[i][1], 0);
-		cout << test[i][1] << endl;
-		cout << Dragon.getName() << endl;
-		cout << Dragon.getLife() << endl;
 		Warrior Ninja(1, 0, test[i][2], 0);
 		Warrior Iceman(2, 0, test[i][3], 0);
 		Warrior Lion(3, 0, test[i][4], 0);
 		Warrior Wolf(4, 0, test[i][5], 0);
 		Warrior blueseq[5] = {Lion, Dragon, Ninja, Iceman, Wolf};
 		Warrior redseq[5] = {Iceman, Lion, Wolf, Ninja, Dragon};
+		int number[2][5] = {0};
 		Foundation Blue(0, test[i][0]);
 		Foundation Red(1, test[i][0]);
-		int posBlue = 0, posRed = 0, count = 0;
-		for (int m = 1; m < 3; m++)
+		int posBlue = findNext(0, Blue, blueseq), posRed = findNext(0, Red, redseq), count = 0;
+		bool flagRed = true, flagBlue = true;
+		while (true)
 		{
-			if (findNext(posRed, Red, redseq) != -1)
+			if (posRed != -1)
 			{
 				posRed = findNext(posRed, Red, redseq);
 				Red.produceWar(redseq[posRed]);
-				cout << count << " red " << redseq[posRed].getName() << ' ' << count + 1 << " born with strength " << redseq[posRed].getLife() << endl;
-				posRed = (posRed + 1) % 5;
+				number[0][posRed]++;
+				cout << setfill('0') << setw(3) << count << " red " << redseq[posRed].getName() << ' ' << count + 1 << " born with strength " << redseq[posRed].getLife()
+						<< ',' << number[0][posRed] << ' ' << redseq[posRed].getName() << " in red headquarter" << endl;
+				posRed = findNext((posRed + 1) % 5, Red, redseq);
 			}
-			if (findNext(posBlue, Blue, blueseq) != -1)
+			else if (flagRed == true)
+			{
+				cout << setfill('0') << setw(3) << count << " red headquarter stops making warriors" << endl;
+				flagRed = false;
+			}
+			if (posBlue != -1)
 			{
 				posBlue = findNext(posBlue, Blue, blueseq);
 				Blue.produceWar(blueseq[posBlue]);
-				cout << Blue.getRes();
-				cout << count << " blue " << blueseq[posBlue].getName() << ' ' << count + 1 << " born with strength " << redseq[posBlue].getLife() << endl;
-				posBlue = (posBlue + 1) % 5;
+				number[1][posBlue]++;
+				cout << setfill('0') << setw(3) << count << " blue " << blueseq[posBlue].getName() << ' ' << count + 1 << " born with strength " << redseq[posBlue].getLife()
+						<< ',' << number[1][posBlue] << ' ' << blueseq[posBlue].getName() << " in blue headquarter" << endl;
+				posBlue = findNext((posBlue + 1) % 5, Blue, blueseq);
 			}
+			else if (flagBlue == true)
+			{
+				cout << setfill('0') << setw(3) << count << " blue headquarter stops making warriors" << endl;
+				flagBlue = false;
+			}
+			if (flagBlue == false && flagRed == false) break;
 			count++;
 		}
 	}
